546554b2dbaca851f9d2744f5b59c345f9637333
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-04-18 17:48:24 +0000

    Add 101

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Given the parameters and the return values,
+add type annotations to the function
+"""
+
+from typing import Any, Mapping, TypeVar, Union
+
+
+def safely_get_value(
+        dct: Mapping, key: Any, default: Union[TypeVar, None] = None
+        ):
+    """ gets value from dictionary safely """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
