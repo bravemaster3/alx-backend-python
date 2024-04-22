9a4e4c042f2e82feb43e459f8ec8a127490ec74c
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-04-22 16:13:17 +0000

    Added sorting

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,13 +5,12 @@ You will spawn wait_random n times with the specified max_delay.
 """
 
 from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Async function that returns a list of n random floats """
-    import asyncio
-    wait_random = __import__('0-basic_async_syntax').wait_random
-
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     completed, pending = await asyncio.wait(tasks)
-    return [task.result() for task in completed]
+    return sorted([task.result() for task in completed])
