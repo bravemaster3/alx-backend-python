32bab971ec8ed8ef36e612e2eaae981a5ba807c4
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-11 08:32:58 +0000

    Add 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -35,3 +35,14 @@ class TestGithubOrgClient(unittest.TestCase):
         result = test_client._public_repos_url
 
         self.assertEqual(result, payload["repos_url"])
+
+    @patch('client.get_json', return_value=[{"name": "repo1"}, {"name": "repo2"}])
+    def test_public_repos(self, mock_get_json):
+        """method to test GithubOrgClient.public_repos"""
+        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = "https://api.github.com/orgs/octocat/repos"
+            test_client = GithubOrgClient("octocat")
+            repos = test_client.public_repos()
+            self.assertEqual(repos, ["repo1", "repo2"])
+            mock_get_json.assert_called_once()
+            mock_public_repos_url.assert_called_once()
