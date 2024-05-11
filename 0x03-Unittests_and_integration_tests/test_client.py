fe0c14e613ddc51a76c3372abd934f10e7b1bf36
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-11 08:35:54 +0000

    Add 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -36,11 +36,15 @@ class TestGithubOrgClient(unittest.TestCase):
 
         self.assertEqual(result, payload["repos_url"])
 
-    @patch('client.get_json', return_value=[{"name": "repo1"}, {"name": "repo2"}])
+    @patch('client.get_json', return_value=[{"name": "repo1"},
+                                            {"name": "repo2"}])
     def test_public_repos(self, mock_get_json):
         """method to test GithubOrgClient.public_repos"""
-        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
-            mock_public_repos_url.return_value = "https://api.github.com/orgs/octocat/repos"
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            str1 = "https://api.github.com/"
+            str2 = "orgs/octocat/repos"
+            mock_public_repos_url.return_value = str1 + str2
             test_client = GithubOrgClient("octocat")
             repos = test_client.public_repos()
             self.assertEqual(repos, ["repo1", "repo2"])
